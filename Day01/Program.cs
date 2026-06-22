6251b2155de1d4b991695e1a578d258d992e3d90
Chetankumar <chetu00753@gmail.com>
2026-06-22 23:18:28 +0530

    first model cll in c#

---
+++ Day01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.ClientModel;
+using DotNetEnv;
+using Microsoft.Extensions.AI;
+using OpenAI;
+
+Env.Load();
+
+var openRouterKey = Environment.GetEnvironmentVariable("OPENROUTER_API_KEY");
+
+try
+{
+    var client = new OpenAIClient(
+        new ApiKeyCredential(openRouterKey),
+        new OpenAIClientOptions { Endpoint = new Uri("https://openrouter.ai/api/v1") }
+    );
+
+    var model="openai/gpt-oss-120b:free";
+
+    IChatClient chat = client
+        .GetChatClient(model)
+        .AsIChatClient();
+
+    var response = await chat.GetResponseAsync("What is agentic AI? Give me one liner answer.");
+
+    Console.WriteLine(response);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
